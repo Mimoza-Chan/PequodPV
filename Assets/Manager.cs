f06821db2e8724470bdc482bb10b28bdbc9f6d24
Mimoza-Chan <god.airu@gmail.com>
2022-04-24 22:53:53 +0900

    add fade between scenes, fixed cinemachine with timeline, update .gitignore

--- Assets/Manager.cs
+++ Assets/Manager.cs
@@ -2,10 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+using UnityEngine.UI;
 public class Manager: MonoBehaviour
 {
+    [SerializeField] Volume m_Volume;
+    [SerializeField] Image fadePanel;
+    [SerializeField] float smoothTime;
+    float currentVelocity;
+    float currentValue;
+    bool startSmoothDamp;
+    bool endSmoothDamp;
+    Vignette vignette;
     public void OnStartPV()
     {
         SceneManager.LoadScene(0);
     }
+
+    public void OnStartFade()
+    {
+        startSmoothDamp = true;
+    }
+
+    void Awake()
+    {
+        VolumeProfile profile = m_Volume.sharedProfile;
+        profile.TryGet<Vignette>(out vignette);
+        startSmoothDamp = false;
+        endSmoothDamp = false;
+        currentValue = 0;
+    }
+
+    public void OnResetFade()
+    {
+        startSmoothDamp = false;
+        vignette.intensity.value = 0;
+        fadePanel.color = new Color(0,0,0,0);
+    }
+
+    public void OnResetFadeOut()
+    {
+        endSmoothDamp = false;
+        vignette.intensity.value = 1;
+        fadePanel.color = new Color(0,0,0,1);
+    }
+
+    public void OnFadeOut()
+    {
+        endSmoothDamp = true;
+    }
+
+    void Update()
+    {
+        if (startSmoothDamp)
+        {
+            if (currentValue > 1) return;
+            currentValue = Mathf.SmoothDamp(currentValue, 1 ,ref currentVelocity, smoothTime);
+            vignette.intensity.value = currentValue;
+            fadePanel.color = new Color(0,0,0,currentValue);
+        }
+        if (endSmoothDamp)
+        {
+            if (currentValue >= 1)
+            {
+                currentValue = 1;
+                return;
+            }
+            currentValue += Time.deltaTime;
+            vignette.intensity.value = 1 - currentValue;
+            fadePanel.color = new Color(0,0,0,1 - currentValue);
+        }
+    }
+
 }
--- Assets/RotCalculation.cs
+++ Assets/RotCalculation.cs
@@ -2,18 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEditor;
+using System;
 
-public class RotCalculation : MonoBehaviour
+
+public class RotCalculation : EditorWindow
 {
-    // Start is called before the first frame update
-    void Start()
+    GameObject planet;
+    int radius;
+    int dividValue;
+    float maxRandomValue;
+
+    [MenuItem("Tools/Calculate")]
+    public static void Excute()
     {
-        
+        RotCalculation window = GetWindow<RotCalculation>();
+        window.titleContent = new GUIContent("Calculation Window");
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnGUI()
     {
-        
+        using (new EditorGUILayout.VerticalScope())
+        {
+            planet = (GameObject)EditorGUILayout.ObjectField("GameObject", planet, typeof(GameObject), true);
+            radius = EditorGUILayout.IntField("Radius", radius);
+            GUILayout.Label("Enter minus value to set the angle over 180");
+            dividValue = EditorGUILayout.IntField("Polygon value", dividValue);
+        }
+        if (GUILayout.Button("Apply"))
+        {
+            planet.transform.position = Calculate(radius, dividValue);
+        }
+    }
+
+    Vector3 Calculate(int r, int n)
+    {
+        return new Vector3( r * MathF.Cos(MathF.PI / n), 0, r * Mathf.Sin(Mathf.PI / n));
     }
 }
